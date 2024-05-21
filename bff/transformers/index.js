7efbd59422596619cfdd9d0acacfe5d6013ebce7
sboevvitalii <140063683+sboevvitalii@users.noreply.github.com>
2024-05-21 18:56:04 +0500

    Delete bff directory

--- bff/transformers/index.js
+++
@@ -1,5 +0,0 @@
-export * from './transform-comment';
-export * from './transform-post';
-export * from './transform-role';
-export * from './transform-session';
-export * from './transform-user';
--- bff/transformers/transform-comment.js
+++
@@ -1,7 +0,0 @@
-export const tsransformComment = (dbComment) => ({
-	id: dbComment.id,
-	postId: dbComment.post_id,
-	authorId: dbComment.autor_id,
-	publishedAt: dbComment.published_at,
-	content: dbComment.content,
-});
