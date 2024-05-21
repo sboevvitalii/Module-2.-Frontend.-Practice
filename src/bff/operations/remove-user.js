1ef7c9be59d4e43ec330a434e977cfce174cc262
sboevvitalii <140063683+sboevvitalii@users.noreply.github.com>
2024-05-21 18:57:07 +0500

    Add files via upload

---
+++ src/bff/operations/remove-user.js
@@ -0,0 +1,22 @@
+import { deleteUser } from '../api';
+import { sessions } from '../sessions';
+import { ROLE } from '../constants';
+
+export const removeUser = async (hash, userId) => {
+	const accessRoles = [ROLE.ADMIN];
+
+	const access = await sessions.access(hash, accessRoles);
+
+	if (!access) {
+		return {
+			error: 'Доступ запрещен',
+			res: null,
+		};
+	}
+
+	deleteUser(userId);
+	return {
+		error: null,
+		res: true,
+	};
+};
